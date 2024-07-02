6e24c0faa3864c30fee054667e368f20d00041b2
Addriank12 <addriank12@gmail.com>
2024-07-02 09:17:21 -0500

    si

--- src/main/java/ups/demo/demo/ups/DemoApplication.java
+++ src/main/java/ups/demo/demo/ups/DemoApplication.java
@@ -1,8 +1,19 @@
 package ups.demo.demo.ups;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class DemoApplication {
+public class DemoApplication extends Application{
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(CORSFilter.class); // Registra el filtro CORS
+        return resources;
+    }
 
 }
